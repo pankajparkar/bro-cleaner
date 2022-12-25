904df3f8fcac678328e1221b43182d1b033e6124
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-12-26 00:13:11 +0800

    Removed unused files.

--- clean.js
+++ clean.js
@@ -1,16 +1,38 @@
-function runBrowserCleaner() {
-    chrome.storage.local.clear(function () {
-        var error = chrome.runtime.lastError;
-        if (error) {
-            console.error(error);
-        } else {
-            console.log('Local Storage is cleaned');
-        }
-    });
+
+function getUrl(currentTab) {
+    const originRegex = /(^https?:\/\/)[^\/]+/gi
+    return currentTab.url.match(originRegex)[0];
 }
 
+async function runBrowserCleaner() {
+    const [currentTab] = chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    });
+    if (currentTab === undefned) return;
+    const currentUrl = getUrl(currentTab);
+    await chrome.browsingData.remove({
+        "origins": [
+            currentUrl
+        ]
+    }, {
+        // "cacheStorage": true,
+        // "cookies": true,
+        // "fileSystems": true,
+        // "indexedDB": true,
+        "localStorage": true,
+        // "sessionStorage": true,
+        // "pluginData": true,
+        // "serviceWorkers": true,
+        // "webSQL": true
+    }, () => {
+        alert(`Cleared site data on ${currentUrl}`);
+        // chrome.tabs.reload();
+    });
+}
+console.log('localStorage', localStorage, window.localStorage);
+
 document.addEventListener('DOMContentLoaded', function () {
-    debugger;
     document.getElementById("run-bro-clean").addEventListener(
         'click',
         runBrowserCleaner
