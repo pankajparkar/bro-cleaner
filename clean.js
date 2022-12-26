6ebc9e3c1203c209c08a90eee94cd695ceef76e3
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-12-26 10:57:26 +0800

    1. Save values 2. Populate values 3. Remove console. 4. Create form controls method. 5. Label for for better UX. 6. CSS added

--- clean.js
+++ clean.js
@@ -1,40 +1,70 @@
 
 function getUrl(currentTab) {
-    const originRegex = /(^https?:\/\/)[^\/]+/gi
-    return currentTab.url.match(originRegex)[0];
+    const originRegex = /(^https?:\/\/)[^\/]+/gi;
+    const matched = currentTab.url.match(originRegex);
+    return matched && matched.length && matched[0];
 }
 
-async function runBrowserCleaner() {
-    const [currentTab] = chrome.tabs.query({
+async function runBrowserCleaner(obj) {
+    const [currentTab] = await chrome.tabs.query({
         active: true,
-        currentWindow: true
+        currentWindow: true,
     });
-    if (currentTab === undefned) return;
+    if (!currentTab) return;
     const currentUrl = getUrl(currentTab);
     await chrome.browsingData.remove({
         "origins": [
             currentUrl
         ]
-    }, {
-        // "cacheStorage": true,
-        // "cookies": true,
-        // "fileSystems": true,
-        // "indexedDB": true,
-        "localStorage": true,
-        // "sessionStorage": true,
-        // "pluginData": true,
-        // "serviceWorkers": true,
-        // "webSQL": true
-    }, () => {
-        alert(`Cleared site data on ${currentUrl}`);
-        // chrome.tabs.reload();
-    });
+    }, obj,
+        // {
+        //     // "cacheStorage": true,
+        //     // "cookies": true,
+        //     // "fileSystems": true,
+        //     // "indexedDB": true,
+        //     "localStorage": true,
+        //     // "sessionStorage": true,
+        //     // "pluginData": true,
+        //     // "serviceWorkers": true,
+        //     // "webSQL": true
+        // }, 
+        () => {
+            alert(`Cleared site data on ${currentUrl}`);
+            // chrome.tabs.reload();
+        });
 }
-console.log('localStorage', localStorage, window.localStorage);
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById("run-bro-clean").addEventListener(
-        'click',
-        runBrowserCleaner
+const getFormElements = () => {
+    return document.querySelector('form[name=browser-cleaner]').elements;
+}
+
+async function formSubmit(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const [localStorageEl, sessionStorageEl, cacheStorageEl, cookiesEl,] = getFormElements();
+    const obj = {
+        localStorage: localStorageEl.checked,
+        // sessionStorage: sessionStorage.checked,
+        cacheStorage: cacheStorageEl.checked,
+        cookies: cookiesEl.checked,
+    };
+    await chrome.storage.local.set(obj);
+    await runBrowserCleaner(obj);
+}
+
+async function prefillValues() {
+    const { localStorage, cacheStorage, cookies } = await chrome.storage.local.get(['localStorage', 'cacheStorage', 'cookies', 'sessionStorage']);
+    const [localStorageEl, sessionStorageEl, cacheStorageEl, cookiesEl,] = getFormElements();
+    localStorageEl.checked = localStorage || false;
+    sessionStorageEl.checked = cacheStorage || false;
+    cacheStorageEl.checked = cookies || false;
+    cookiesEl.checked = cookies || false;
+}
+
+document.addEventListener('DOMContentLoaded', async function () {
+    await prefillValues();
+    document.querySelector("[name=browser-cleaner]").addEventListener(
+        'submit',
+        formSubmit
     );
 });
